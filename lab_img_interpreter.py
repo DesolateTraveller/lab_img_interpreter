82a9ac1422c941ee3e035097ab1e6954eeceee14
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-09-18 12:14:48 +0530

    Update lab_img_interpreter.py

--- lab_img_interpreter.py
+++ lab_img_interpreter.py
@@ -131,6 +131,7 @@ if uploaded_file is not None:
 
             image = Image.open(uploaded_file)
             img_array = np.array(image)
+            st.image(img_array, caption="Uploaded Image", use_column_width=True)
 
         with col2:
                 
